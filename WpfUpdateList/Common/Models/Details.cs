ae53a1db6995c44948f61eb64a3b225c2d9fff57
Sir Rufo <oliver.muenzberg@gmail.com>
2022-01-27 22:26:43 +0100

    Projektdateien hinzufügen.

---
+++ WpfUpdateList/Common/Models/Details.cs
@@ -0,0 +1,30 @@
+﻿namespace WpfUpdateList.Common.Models
+{
+    public class Details : ReactiveObject
+    {
+        [Reactive] public string Name { get; set; } = string.Empty;
+        [Reactive] public string Company { get; set; } = string.Empty;
+        [Reactive] public string Region { get; set; } = string.Empty;
+        [Reactive] public string HRA { get; set; } = string.Empty;
+        [Reactive] public string Basic { get; set; } = string.Empty;
+        [Reactive] public string PF { get; set; } = string.Empty;
+        [Reactive] public string Salary { get; set; } = string.Empty;
+
+        public void CopyTo(Details other)
+        {
+            other.Name = Name;
+            other.Company = Company;
+            other.Region = Region;
+            other.HRA = HRA;
+            other.Basic = Basic;
+            other.PF = PF;
+            other.Salary = Salary;
+        }
+
+        public void CopyFrom(Details? other)
+        {
+            other ??= new Details();
+            other.CopyTo(this);
+        }
+    }
+}
---
+++ WpfUpdateList/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+
+using WpfUpdateList.Common.Models;
+
+namespace WpfUpdateList.ViewModels
+{
+    public class DetailsViewModel : ReactiveObject
+    {
+        private readonly ObservableAsPropertyHelper<bool> recordIsValid;
+
+        public Details Record { get; } = new();
+        public ObservableCollection<Details> Records { get; } = new();
+        [Reactive] public Details? SelectedRecord { get; set; }
+        public bool RecordIsValid => recordIsValid.Value;
+        public ReactiveCommand<Unit, Unit> SubmitCommand { get; }
+        public ReactiveCommand<Unit, Unit> UpdateCommand { get; }
+        public ReactiveCommand<Unit, Unit> RemoveCommand { get; }
+
+        public DetailsViewModel()
+        {
+            // copy selected record content to edit record
+            this.WhenAnyValue(e => e.SelectedRecord)
+                .WhereNotNull()
+                .Subscribe(e => Record.CopyFrom(e));
+
+            recordIsValid = Record
+                .WhenAny(
+                    e => e.Name,
+                    e => e.Company,
+                    (name, company) => (Name: name.Value, Company: company.Value))
+                .Select(e => !string.IsNullOrEmpty(e.Name) && !string.IsNullOrEmpty(e.Company))
+                .ToProperty(this, e => e.RecordIsValid);
+
+            var canSubmit = this.WhenAnyValue(e => e.RecordIsValid);
+            var canUpdate = this.WhenAny(e => e.RecordIsValid, e => e.SelectedRecord, (valid, record) => valid.Value && record.Value is not null);
+            var canRemove = this.WhenAny(e => e.SelectedRecord, e => e.Value is not null);
+
+            SubmitCommand = ReactiveCommand.Create(DoSubmit, canSubmit);
+            UpdateCommand = ReactiveCommand.Create(DoUpdate, canUpdate);
+            RemoveCommand = ReactiveCommand.Create(DoRemove, canRemove);
+        }
+
+        private void DoSubmit()
+        {
+            var temp = new Details();
+            Record.CopyTo(temp);
+            Records.Add(temp);
+            SelectedRecord = temp;
+        }
+
+        private void DoUpdate() => Record.CopyTo(SelectedRecord!);
+
+        private void DoRemove()
+        {
+            var selectedIndex = Records.IndexOf(SelectedRecord!);
+            Records.Remove(SelectedRecord!);
+            if (Records.Any())
+            {
+                if (Records.Count == selectedIndex)
+                    SelectedRecord = Records.Last();
+                else
+                    SelectedRecord = Records[selectedIndex];
+            }
+        }
+    }
+}
